534fc0b92869c3ef48cee9a4564524ead6ec0f24
BehrangBina <BehrangBina@hotmail.com>
2016-11-06 13:51:40 +1100

    IncrementDecrement

--- CSharpFeatures/IncrementDecrement.cs
+++ CSharpFeatures/IncrementDecrement.cs
@@ -1,29 +1,18 @@
 ﻿using System;
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharpFeatures
 {
-    public class Program
+    public class IncrementDecrement
     {
-        public static void Main(string[] args)
-        {
-            Increment(10);
-            Decrement(10);
-            Console.Read();
-        }
-
         public static void Increment(int originalValue)
         {
             // Test Increment 
-            Console.WriteLine($"Original Values= Value1:{originalValue} | value2={originalValue}");
+            Console.WriteLine($"Calculating on {originalValue}");
 
             var temp1 = ++originalValue;
             Console.WriteLine($"Value:{originalValue} | PlaceHolde={temp1} | ++Value");
 
+            Console.WriteLine($"Calculating on {originalValue}");
             var temp2 = originalValue++;
             Console.WriteLine($"Value:{originalValue} | PlaceHolde={temp2} | Value++");
         }
@@ -31,11 +20,12 @@ namespace CSharpFeatures
         public static void Decrement(int originalValue)
         {
             // Test  Decrement
-            Console.WriteLine($"Original Values= Value1:{originalValue} | value2={originalValue}");
+            Console.WriteLine($"Calculating on {originalValue}");
 
             var temp1 = --originalValue;
             Console.WriteLine($"Value:{originalValue} | PlaceHolde={temp1} | --Value");
 
+            Console.WriteLine($"Calculating on {originalValue}");
             var temp2 = originalValue--;
             Console.WriteLine($"Value:{originalValue} | PlaceHolde={temp2} | Value--");
         }
