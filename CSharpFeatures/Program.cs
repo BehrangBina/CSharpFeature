ffd6739fe1af46aeaf1e2cb77480b2f00e05be21
BehrangBina <BehrangBina@hotmail.com>
2016-11-06 14:17:42 +1100

    Operators

--- CSharpFeatures/Program.cs
+++ CSharpFeatures/Program.cs
@@ -11,33 +11,9 @@ namespace CSharpFeatures
     {
         public static void Main(string[] args)
         {
-            Increment(10);
-            Decrement(10);
+            IncrementDecrement.Increment(10);
+            IncrementDecrement.Decrement(10);
             Console.Read();
-        }
-
-        public static void Increment(int originalValue)
-        {
-            // Test Increment 
-            Console.WriteLine($"Original Values= Value1:{originalValue} | value2={originalValue}");
-
-            var temp1 = ++originalValue;
-            Console.WriteLine($"Value:{originalValue} | PlaceHolde={temp1} | ++Value");
-
-            var temp2 = originalValue++;
-            Console.WriteLine($"Value:{originalValue} | PlaceHolde={temp2} | Value++");
-        }
-
-        public static void Decrement(int originalValue)
-        {
-            // Test  Decrement
-            Console.WriteLine($"Original Values= Value1:{originalValue} | value2={originalValue}");
-
-            var temp1 = --originalValue;
-            Console.WriteLine($"Value:{originalValue} | PlaceHolde={temp1} | --Value");
-
-            var temp2 = originalValue--;
-            Console.WriteLine($"Value:{originalValue} | PlaceHolde={temp2} | Value--");
         }
     }
 }
