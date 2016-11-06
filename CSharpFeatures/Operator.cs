b34f4e700699a8c88e1ff382c46909d2df5bb00a
BehrangBina <BehrangBina@hotmail.com>
2016-11-06 14:16:44 +1100

    Operators

---
+++ CSharpFeatures/Operator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFeatures
+{
+    public static class Operator
+    {
+        public static bool DoubleEqual(bool a, int b, int c)
+        {
+            return a = b == c; 
+        }
+        public static bool EqualEsclation(bool a, int b, int c)
+        {
+            return a = b != c;
+        }
+        public static bool LessThan(bool a, int b, int c)
+        {
+            return a = b<c;
+        }
+        public static bool GreaterThan(bool a, int b, int c)
+        {
+            return a = b > c;
+        }
+        public static bool LessThanEqual(bool a, int b, int c)
+        {
+            return a = b <= c;
+        }
+        public static bool GreaterThanEqual(bool a, int b, int c)
+        {
+            return a = b >= c;
+        }
+    }
+}
